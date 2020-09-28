1904e7db355876dbf7b67e4ab4cb2ec75f751dcd
florin-avram <avram.florin.c@gmail.com>
2020-09-28 15:08:54 +0200

    React navigation part I. Fix issues with the react-navigation package

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -6,38 +6,37 @@ import { DISHES } from '../shared/dishes';
 function RenderDish(props) {
 
     const dish = props.dish;
-    if (dish != null) {
-        return(
-            <Card>
-                <Card.Title> {dish.name} </Card.Title>
-                <Card.Image source={require('./images/uthappizza.png')} />
-                <Text style={{margin: 10}}>
-                    {dish.description}
-                </Text>
-            </Card>
-        );
-    } else {
-        return(<View></View>);
-    }
+    
+        if (dish != null) {
+            return(
+                <Card
+                featuredTitle={dish.name}
+                image={require('./images/uthappizza.png')}>
+                    <Text style={{margin: 10}}>
+                        {dish.description}
+                    </Text>
+                </Card>
+            );
+        }
+        else {
+            return(<View></View>);
+        }
 }
 
 class DishDetail extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            dishes: DISHES,
-        }
-    }
-
-    static navigationOptions = {
-        title: 'Dish Details',
+            dishes: DISHES
+        };
     }
 
     render() {
-        const dishId = this.props.navigation.getParam('dishId', '');
-
-        return(<RenderDish dish={this.state.dishes[+dishId] }/>);
+        const dishId = this.props.route.params.dishId;
+        return(
+            <RenderDish dish={this.state.dishes[+dishId]} />
+        );
     }
 }
-
 export default DishDetail;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
+import { View, Platform } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
-import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation-stack';
-import Constants from 'expo-constants';
-import { createAppContainer } from 'react-navigation';
 
-const MenuNavigator = createStackNavigator({
-    Menu: { screen: Menu },
-    DishDetail: { screen: DishDetail},
-}, {
-    initialRouteName: 'Menu',
-    navigationOptions: {
-        headerStyle: {
-            backgroundColor: '#512DA8'
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-            color: '#fff'
-        }
-    }
-});
+const MenuNavigator = createStackNavigator();
 
-const Main = createAppContainer(MenuNavigator);
+function MenuNavigatorScreen() {
+    return(
+        <MenuNavigator.Navigator
+            initialRouteName='Menu'
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#512DA8"
+                },
+                headerTintColor: "#fff",
+                headerTitleStyle: {
+                    color: "#fff"            
+                }
+            }}
+        >
+            <MenuNavigator.Screen
+                name="Menu"
+                component={Menu}
+            />
+            <MenuNavigator.Screen
+                name="DishDetail"
+                component={DishDetail}
+                options={{ headerTitle: "Dish Detail"}}
+            />            
+        </MenuNavigator.Navigator>
+    );
+}
 
+class Main extends Component {
+
+  render() {
+ 
+    return (
+        <NavigationContainer>
+            <MenuNavigatorScreen/>           
+        </NavigationContainer>
+    );
+  }
+}
+  
 export default Main;
