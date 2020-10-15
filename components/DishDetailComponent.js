502f6d0b88f3a9dd8d4d00e0e99a6a78fe6f0c0e
florin-avram <avram.florin.c@gmail.com>
2020-10-15 15:23:56 +0200

    Assignment 3 part III. Add a left to right swipe gesture panhandler to open the add a comment form when the user executes the gesture

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -24,26 +24,32 @@ const mapDispatchToProps = dispatch => ({
 
 function RenderDish(props) {
     const dish = props.dish;
-    handleViewRef = ref => this.view = ref;
 
-    const recognizeDrag = ({moveX, moveY, dx, dy}) => {
+    const recognizeRightToLeftDrag = ({moveX, moveY, dx, dy}) => {
         if (dx < -200)
             return true;
-        else
-            return false;
+        return false;
     }
+
+    const recognizeLeftToRightDrag = ({moveX, moveY, dx, dy}) => {
+        if (dx > 200)
+            return true;
+        return false;
+    }
+
+    let viewRef;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder:
             (e, gestureState) => { return true; },
         onPanResponderGrant:
             () => {
-                this.view.rubberBand(1000)
+                viewRef.rubberBand(1000)
                     .then(endState => console.log(endState.finished ? ' finished' : 'cancelled'))
             },
         onPanResponderEnd:
             (e, gestureState) => {
-                if (recognizeDrag(gestureState))
+                if (recognizeRightToLeftDrag(gestureState))
                     Alert.alert(
                         'Add to Favorites',
                         'Are you sure you wish to add ' + dish.name + ' to your favorites?',
@@ -61,6 +67,9 @@ function RenderDish(props) {
                         ],
                         { cancelable: false}
                     )
+                if (recognizeLeftToRightDrag(gestureState)) {
+                    props.toggleModal();
+                }
                 return true;
             },
     })
@@ -71,8 +80,8 @@ function RenderDish(props) {
                 animation="fadeInDown"
                 duration={2000}
                 delay={1000}
-                ref={this.handleViewRef}
                 {...panResponder.panHandlers}
+                ref={(ref) => { viewRef = ref }}
             >
                 <Card>
                     <Card.Title> {dish.name} </Card.Title>
